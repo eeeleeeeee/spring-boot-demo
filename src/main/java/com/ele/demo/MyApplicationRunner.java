f6637498f62774da042387c61098df72da547bca
EugeneTei <113669967+EugeneTei@users.noreply.github.com>
2024-08-20 15:26:26 +0800

    feat: After processing a specific task, shut down Spring Boot

--- src/main/java/com/ele/demo/MyApplicationRunner.java
+++ src/main/java/com/ele/demo/MyApplicationRunner.java
@@ -1,18 +1,34 @@
 package com.ele.demo;
 
-import lombok.extern.flogger.Flogger;
-import lombok.extern.log4j.Log4j2;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Component;
 
-@Log4j2
+@Slf4j
+@RequiredArgsConstructor
 @Component
 public class MyApplicationRunner implements ApplicationRunner {
 
+    private final ConfigurableApplicationContext configurableApplicationContext;
+
+    private final static String JOB = "JOB";
+
     @Override
-    public void run(ApplicationArguments args) throws Exception {
-        log.info("--- Non option args ---");
-        args.getNonOptionArgs().forEach(log::info);
+    public void run(ApplicationArguments args) {
+        var jobValues = args.getOptionValues(JOB);
+        if (jobValues != null && jobValues.size() > 0) {
+            String jobName = jobValues.get(0);
+            try {
+                log.info("Run the task {} start.", jobName);
+                log.info("Run the task {} end.", jobName);
+            } catch (Exception e) {
+                log.error("The task failed to run.", e);
+            } finally {
+                configurableApplicationContext.close();
+            }
+        }
     }
 }
--- src/main/java/com/ele/demo/MyCommandLineRunner.java
+++ src/main/java/com/ele/demo/MyCommandLineRunner.java
@@ -1,10 +1,10 @@
 package com.ele.demo;
 
-import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-@Log4j2
+@Slf4j
 @Component
 public class MyCommandLineRunner implements CommandLineRunner {
 
