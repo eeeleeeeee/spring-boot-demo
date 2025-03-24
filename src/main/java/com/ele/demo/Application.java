884b9a51315c72d42b67182c5ba136ec9d2392d7
TW - IT - Eugene Zheng <113669967+EugeneTei@users.noreply.github.com>
2025-03-24 16:54:04 +0800

    feat: test controller to verify the app is running correctly

--- src/main/java/com/ele/demo/Application.java
+++ src/main/java/com/ele/demo/Application.java
@@ -6,10 +6,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-public class SpringBootDemoApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDemoApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
-
 }
